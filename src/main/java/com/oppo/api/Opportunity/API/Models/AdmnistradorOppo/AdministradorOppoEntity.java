67ab9ef0cb56202c1b2aeb62acbf3f73d129d6eb
Erickson Dias <103853894+EricksonLOOP@users.noreply.github.com>
2024-10-01 16:41:20 -0300

    created: Nova entidade - ADM

---
+++ src/main/java/com/oppo/api/Opportunity/API/Models/AdmnistradorOppo/AdministradorOppoEntity.java
@@ -0,0 +1,23 @@
+package com.oppo.api.Opportunity.API.Models.AdmnistradorOppo;
+
+import com.oppo.api.Opportunity.API.Models.TagsENUM;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Entity
+@Data
+public class AdministradorOppoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String nome;
+    private String cpf;
+    private String rg;
+    private TagsENUM role;
+
+}
